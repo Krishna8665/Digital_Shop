ff3c119cd420a5e691c5e2eb7faf08d19f7278b4
Krishna8665 <khatrikrissna11@gmail.com>
2025-09-18 14:49:36 +0545

    controller

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import './database/connection'
+import "./database/connection";
+import userRoute from './routes/userRoutes'
 const app = express();
+
+app.use(express.json());
+app.use("/api/auth",userRoute)
 
 export default app;
